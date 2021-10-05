a89bda766c75816cb48a1007cd7a4e712f383233
Uislan Cruz <uislansk8@hotmail.com>
2021-10-04 22:00:25 -0300

    Op
    Testando uns Op

--- OperadorTernario/src/operadorternario/OperadorTernario.java
+++ OperadorTernario/src/operadorternario/OperadorTernario.java
@@ -16,11 +16,18 @@ public class OperadorTernario {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int n1, n2, r;
+        /* int n1, n2, r;
         n1 = 14;
         n2 = 8;
-        r = (n1>n2)?0:1;
-        System.out.println(r);
+        r = (n1>n2)? 0:1;
+        System.out.println(r);*/
+        
+        String nome1 = "Gustavo";
+        String nome2 = "Gustavo";
+        String nome3 = new String ("Gustavo");
+        //String res = (nome1.equals(nome3))?"igual":"diferente";//confere o conteudo
+        String res = (nome1==nome2)?"igual":"diferente";
+        System.out.println(res);
     }
     
 }
